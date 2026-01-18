222fe9954d9ce3d43895b4a5f23f809602bf40a4
Kai Waldrant <kai.waldrant@outlook.com>
2026-01-18 22:29:23 +0100

    Add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Space_Mono } from "next/font/google";
+
+export const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+});
+
+export const spaceMono = Space_Mono({
+    weight: ["400", "700"],
+    variable: "--font-space-mono",
+    subsets: ["latin"],
+});
