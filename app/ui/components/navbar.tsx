1582c4f8d4f420469f9a39c3556ea14b086ecbd6
Kai Waldrant <kai.waldrant@outlook.com>
2026-01-21 22:25:50 +0100

    WIP add styling theme

--- app/ui/components/navbar.tsx
+++ app/ui/components/navbar.tsx
@@ -5,12 +5,14 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <nav className="flex items-center w-full">
-      <div className="flex justify-between w-296">
-        <Image className="md:hidden" src={Logo} alt="Logo" />
-        <Image className="hidden md:block h-8" src={LogoBig} alt="Logo" />
-        <NavLinks />
-      </div>
-    </nav>
+    <header className="sticky content-grid ">
+      <nav className="flex items-center w-full">
+        <div className="flex justify-between w-296">
+          <Image className="md:hidden" src={Logo} alt="Logo" />
+          <Image className="hidden md:block h-8" src={LogoBig} alt="Logo" />
+          <NavLinks />
+        </div>
+      </nav>
+    </header>
   );
 }
