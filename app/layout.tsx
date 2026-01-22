39d82697149312e549bf3cf291f12af15017efa0
Kai Waldrant <kai.waldrant@outlook.com>
2026-01-22 21:44:19 +0100

    Add theme toggle and chadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,31 @@
 import type { Metadata } from "next";
 import { inter, spaceMono } from "./ui/fonts";
 import "./globals.css";
+import { ThemeProvider } from "./ui/theme-provider";
 
 export const metadata: Metadata = {
-    title: "Kai Waldrant software engineer, fullstack developer, bioinformatician",
-    description: "The Resume and portfolio for Kai Waldrant.",
+  title:
+    "Kai Waldrant software engineer, fullstack developer, bioinformatician",
+  description: "The Resume and portfolio for Kai Waldrant.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.variable} ${spaceMono.variable} antialiased`}
-            >
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} ${spaceMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
--- app/ui/components/navbar.tsx
+++ app/ui/components/navbar.tsx
@@ -2,6 +2,7 @@ import NavLinks from "./nav-links";
 import Logo from "@/public/logo.svg";
 import LogoBig from "@/public/logo_big.svg";
 import Image from "next/image";
+import ThemeToggle from "./theme-toggle";
 
 export default function NavBar() {
   return (
@@ -11,6 +12,7 @@ export default function NavBar() {
           <Image className="md:hidden" src={Logo} alt="Logo" />
           <Image className="hidden md:block h-8" src={LogoBig} alt="Logo" />
           <NavLinks />
+          <ThemeToggle />
         </div>
       </nav>
     </header>
