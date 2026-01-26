10eba9c54305094e5f27c24925132af26d5fab29
Kai Waldrant <kai.waldrant@outlook.com>
2026-01-26 22:43:46 +0100

    Add margin to section

--- components/Section.tsx
+++ components/Section.tsx
@@ -15,7 +15,7 @@ export default function Section({
   ...props
 }: SectionProps) {
   return (
-    <section className={cn("content-grid full", className)} {...props}>
+    <section className={cn("content-grid full my-24", className)} {...props}>
       {container ? (
         <div className={cn("", containerClassName)}>{children}</div>
       ) : (
