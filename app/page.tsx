0ba22f92e903be6c1749893d113a8c71028bddf8
Kai Waldrant <kai.waldrant@outlook.com>
2026-01-29 23:05:09 +0100

    WIP Skills.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/components/navbar";
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
+import Skills from "@/components/sections/Skills";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="content-grid">
         <Hero />
         <About />
+        <Skills />
       </main>
     </>
   );
---
+++ components/sections/Skills.tsx
@@ -0,0 +1,21 @@
+import Section from "../Section";
+
+const devStack = [];
+const domains = [];
+
+export default function Skills() {
+  return (
+    <Section containerClassName="grid">
+      <h2 className="font-mono text-4xl text-center">Skills</h2>
+      <div>
+        <div>
+          <h3>Development Stack</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3>Domain Expertise</h3>
+        </div>
+      </div>
+    </Section>
+  );
+}
