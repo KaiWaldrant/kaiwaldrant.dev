c1a029b7b8f247636d4f3f10772ad5bb30201ace
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-02 14:39:52 +0100

    Update Contact

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -12,10 +12,12 @@ export default function Contact() {
         <h2 className="font-mono text-4xl font-bold text-center">Get in Touch</h2>
         <p className="text-text-secondary text-lg">Lets connect and collaborate</p>
       </div>
-      <div className="grid md:grid-cols-2 gap-16">
-        <div className="grid gap-6">
-          <h3 className="text-2xl font-bold">Contact Information</h3>
-          <p className="text-text-secondary">Feel free to reach out via email or connect with me on social media. I'm always open to discussing new projects, job opportunities, or interesting ideas.</p>
+      <div className="grid gap-16">
+        <div className="grid md:grid-cols-2 gap-6">
+          <div className="grid gap-2">
+            <h3 className="text-2xl font-bold">Contact Information</h3>
+            <p className="text-text-secondary">Feel free to reach out via email or connect with me on social media. I'm always open to discussing new projects, job opportunities, or interesting ideas.</p>
+          </div>
           <div className="flex flex-col gap-4">
             <div className="flex gap-2 bg-background-secondary p-2">
               <MailIcon />
@@ -25,20 +27,20 @@ export default function Contact() {
             </div>
             <div className="flex gap-2 bg-background-secondary p-2">
               <GithubIcon  />
-              <Link href="https://github.com/kaiwaldrant">
-                kaiwaldrant
+              <Link href="https://github.com/kaiwaldrant" target="_blank">
+              github.com/kaiwaldrant
               </Link>
             </div>
             <div className="flex gap-2 bg-background-secondary p-2">
         
             <LinkedinIcon />
-              <Link href="https://linkedin.com/in/kaiwaldrant">
-                kaiwaldrant
+              <Link href="https://linkedin.com/in/kai-waldrant" target="_blank">
+              linkedin.com/in/kai-waldrant
               </Link>
             </div>
           </div>
         </div>
-        <div className="grid gap-6">
+        <div className="grid gap-6 hidden">
           <h3 className="text-2xl font-bold">Send a Message</h3>
           <div>
 
