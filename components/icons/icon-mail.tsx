66553d035e2c0063ca760381def252ee7c04e812
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-02 10:14:19 +0100

    Add icon mail

--- components/icons/icon-mail.tsx
+++ components/icons/icon-mail.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const GithubIcon = ({
+const MailIcon = ({
   size = undefined,
   color = '#000000',
   strokeWidth = 2,
@@ -39,10 +39,9 @@ const GithubIcon = ({
         backgroundColor: background !== 'transparent' ? background : undefined
       }}
     >
-      <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth={strokeWidth} d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2c2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2a4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6c-.6.6-.6 1.2-.5 2V21"/>
+      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth={strokeWidth}><path d="m22 7l-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect width="20" height="16" x="2" y="4" rx="2"/></g>
     </svg>
   );
 };
 
-export default GithubIcon;
-
+export default MailIcon;
