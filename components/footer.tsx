4f57b0ce7f1974ac68879d67df2d2fe1548b03fe
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-04 22:50:40 +0100

    Change color of icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,19 +6,25 @@ import GithubIcon from "./icons/icon-github";
 import LinkedinIcon from "./icons/icon-linkedin";
 
 export default function Footer() {
-    return(
-        <footer className="content-grid bg-surface-cards h-48 max-sm:py-6 md:h-18">
-            <div className="flex max-sm:flex-col justify-between md:items-center">
-                <Link key="Logo" href="#" className="flex items-center w-auto">
-                    <Image className="h-6 w-auto" src={LogoBig} alt="Logo" />
-                </Link>
-                <p>© Kai Waldrant 2026. All rigths reserved.</p>
-                <div className="flex gap-4">
-                    <Link href="mailto:kai@kaiwaldrant.dev"><MailIcon /></Link>
-                    <Link href="https://github.com/kaiwaldrant" target="_blank"><GithubIcon /></Link>
-                    <Link href="https://linkedin.com/in/kai-waldrant" target="_blank"><LinkedinIcon /></Link>
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="content-grid bg-surface-cards h-48 max-sm:py-6 md:h-18">
+      <div className="flex max-sm:flex-col justify-between md:items-center">
+        <Link key="Logo" href="#" className="flex items-center w-auto">
+          <Image className="h-6 w-auto" src={LogoBig} alt="Logo" />
+        </Link>
+        <p>&copy; Kai Waldrant 2026. All rigths reserved.</p>
+        <div className="flex gap-4 text-secondary-accent">
+          <Link href="mailto:kai@kaiwaldrant.dev">
+            <MailIcon />
+          </Link>
+          <Link href="https://github.com/kaiwaldrant" target="_blank">
+            <GithubIcon />
+          </Link>
+          <Link href="https://linkedin.com/in/kai-waldrant" target="_blank">
+            <LinkedinIcon />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -4,48 +4,55 @@ import GithubIcon from "../icons/icon-github";
 import LinkedinIcon from "../icons/icon-linkedin";
 import Link from "next/link";
 
-
 export default function Contact() {
   return (
     <Section id="contact" containerClassName="grid gap-12">
       <div className="text-secondary text-center grid gap-4">
-        <h2 className="font-mono text-4xl font-bold text-center">Get in Touch</h2>
-        <p className="text-text-secondary text-lg">Lets connect and collaborate</p>
+        <h2 className="font-mono text-4xl font-bold text-center">
+          Get in Touch
+        </h2>
+        <p className="text-text-secondary text-lg">
+          Lets connect and collaborate
+        </p>
       </div>
       <div className="grid gap-16">
         <div className="grid md:grid-cols-2 gap-6">
           <div className="grid gap-2">
             <h3 className="text-2xl font-bold">Contact Information</h3>
-            <p className="text-text-secondary">Feel free to reach out via email or connect with me on social media. I'm always open to discussing new projects, job opportunities, or interesting ideas.</p>
+            <p className="text-text-secondary">
+              Feel free to reach out via email or connect with me on social
+              media. I&apos;m always open to discussing new projects, job
+              opportunities, or interesting ideas.
+            </p>
           </div>
           <div className="flex flex-col gap-4">
             <div className="flex gap-2 bg-background-secondary p-2">
-              <MailIcon />
-              <Link href="mailto:kai@kaiwaldrant.dev">
-                kai@kaiwaldrant.dev
-              </Link>
+              <span className="text-secondary-accent">
+                <MailIcon />
+              </span>
+              <Link href="mailto:kai@kaiwaldrant.dev">kai@kaiwaldrant.dev</Link>
             </div>
             <div className="flex gap-2 bg-background-secondary p-2">
-              <GithubIcon  />
+              <span className="text-secondary-accent">
+                <GithubIcon color="text-secondary-accent" />
+              </span>
               <Link href="https://github.com/kaiwaldrant" target="_blank">
-              github.com/kaiwaldrant
+                github.com/kaiwaldrant
               </Link>
             </div>
             <div className="flex gap-2 bg-background-secondary p-2">
-        
-            <LinkedinIcon />
+              <span className="text-secondary-accent">
+                <LinkedinIcon color="text-secondary-accent" />
+              </span>
               <Link href="https://linkedin.com/in/kai-waldrant" target="_blank">
-              linkedin.com/in/kai-waldrant
+                linkedin.com/in/kai-waldrant
               </Link>
             </div>
           </div>
         </div>
-        <div className="grid gap-6 hidden">
+        <div className="gap-6 hidden">
           <h3 className="text-2xl font-bold">Send a Message</h3>
-          <div>
-
-          </div>
-
+          <div></div>
         </div>
       </div>
     </Section>
