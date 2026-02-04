22d446567463eb405cac3e6a3ea5cd448fdbbb99
Kai Waldrant <kai.waldrant@outlook.com>
2026-02-04 15:36:51 +0100

    WIP add Projects

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,7 +1,15 @@
-import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardAction, CardFooter } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardFooter,
+} from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import SkillBadge from "./skill-badge";
 
@@ -13,24 +21,47 @@ interface ProjectCardProps {
   skills?: string[];
 }
 
-export default function ProjectCard({ title, description, image, link="#", skills }: ProjectCardProps) {
+export default function ProjectCard({
+  title,
+  description,
+  image,
+  link = "#",
+  skills,
+}: ProjectCardProps) {
   return (
-    <Card className="bg-surface-cards border-border">
-      <CardHeader>
-        {image && <Image src={image} alt={title} width={100} height={100} />}
+    <Card className="bg-surface-cards border-border pt-0 ring-0 shadow-lg shadow-primary-hover/20">
+      <CardHeader className="px-0 min-h-30">
+        {image && (
+          <Image
+            className="overflow-clip"
+            src={image}
+            alt={title}
+            width={1293}
+            height={120}
+          />
+        )}
       </CardHeader>
-      <CardContent>
+      <CardContent className="grid gap-4">
         <CardTitle>{title}</CardTitle>
         <CardDescription>{description}</CardDescription>
         <div className="flex flex-wrap gap-3">
-          {skills && skills.map((skill) => (
-            <SkillBadge key={skill} skill={skill} variant="blue" />
-          ))}
+          {skills &&
+            skills.map((skill) => (
+              <SkillBadge key={skill} skill={skill} variant="blue" />
+            ))}
         </div>
       </CardContent>
-      <CardFooter className="border-none px-2">
+      <CardFooter className="border-none">
         <CardAction>
-            <Button variant="link" asChild><Link href={link}>View Details <ArrowRight /></Link></Button>
+          <Button
+            className="bg-transparent border-primary-accent hover:bg-primary-hover text-primary-accent hover:text-text-primary"
+            variant="outline"
+            asChild
+          >
+            <Link href={link}>
+              View Details <ArrowRight />
+            </Link>
+          </Button>
         </CardAction>
       </CardFooter>
     </Card>
--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -6,9 +6,34 @@ export default function Projects() {
     <Section id="projects" containerClassName="grid gap-12">
       <h2 className="font-mono text-4xl font-bold text-center">Projects</h2>
       <div className="grid md:grid-cols-3 gap-8">
-        <ProjectCard title="DNAnexus Platform" description="Description 1" image="/images/dna.png" link="#" />
-        <ProjectCard title="Genomics Workflows" description="Description 2" image="/images/genomics.png" link="#" />
-        <ProjectCard title="Data Engineering" description="Description 3" image="/images/data.png" link="#" />
+        <ProjectCard
+          title="OpenProblems"
+          description="Benchmarking website for SC RNA-seq methods"
+          image="/openproblems-thumb.jpg"
+          link="https://openproblems.bio/"
+          skills={["SolidJS", "Quarto", "TypeScript"]}
+        />
+        <ProjectCard
+          title="Quiz app"
+          description="Frontend Mentor project. Interactive Quiz app."
+          image="/fem-quiz-thumb.jpg"
+          link="#"
+          skills={[
+            "React",
+            "Vite",
+            "TypeScript",
+            "TailwindCSS",
+            "Frontend Mentor",
+            "Netlify",
+          ]}
+        />
+        <ProjectCard
+          title="Password Generator"
+          description="Frontend Mentor project. A simple password generator"
+          image="/fem-pswgen-thumb.jpg"
+          link="#"
+          skills={["HTML", "CSS", "JavaScript", "Netlify"]}
+        />
       </div>
     </Section>
   );
